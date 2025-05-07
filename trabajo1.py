4325b6c6edfee4bdc358a45aa878482d25d64e9a
nehuengb <gonzalezbuninnehuen15@gmail.com>
2025-05-07 04:03:50 +0000

    Agregada función multiplicación

--- trabajo1.py
+++ trabajo1.py
@@ -2,11 +2,21 @@ from datetime import date
 #parte Juan.
 def saludar():
     print("Hola, esta es la función de Juan.")
+
 #parte Esteban.
-fecha_hoy = date.today()
-print("Fecha de hoy:", fecha_hoy)
+def fechaHoy():
+    fecha_hoy = date.today()
+    print("Fecha de hoy:", fecha_hoy)
 
 #parte Lucas
-print("Saliendo del programa.")
+def salir():
+    print("Saliendo del programa.")
 
-    
+def multiplicacion():
+    numero1 = float(input("Ingresa el primer número: "))
+
+    numero2 = float(input("Ingresa el segundo número: "))
+
+    multiplicacion = numero1 * numero2
+
+    print("La multiplicación de ", numero1, " y ", numero2, " es ", multiplicacion)
