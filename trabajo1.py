1fa71cdaecc13893c8a1f0d71ac79a4694181238
EstebanSoria1 <estebanmsoria@gmail.com>
2025-05-07 00:05:42 +0000

    Agregada función imprimir fecha.

--- trabajo1.py
+++ trabajo1.py
@@ -1,3 +1,9 @@
+from datetime import date
+#parte Juan.
 def saludar():
     print("Hola, esta es la función de Juan.")
+#parte Esteban.
+fecha_hoy = date.today()
+print("Fecha de hoy:", fecha_hoy)
 
+    
