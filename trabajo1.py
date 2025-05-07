4139c5906e7578d976eb51f40824a3bd14cfa050
pedrobalsari <balsaripedro@gmail.com>
2025-05-07 10:19:17 -0300

    Update trabajo1.py

--- trabajo1.py
+++ trabajo1.py
@@ -9,4 +9,11 @@ print("Fecha de hoy:", fecha_hoy)
 #parte Lucas
 print("Saliendo del programa.")
 
-    
+#parte pedro
+numero1 = float(input("Ingrese el primer número: "))
+
+numero2 = float(input("Ingrese el segundo número: "))
+
+suma = numero1 + numero2
+
+print(f"La suma de {numero1} y {numero2} es {suma}
